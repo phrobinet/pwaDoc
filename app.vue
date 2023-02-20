4bad93648f47350ce17c66638d8c6db4bdef1163
phrobinet <ph.robinet@gmail.com>
2023-02-20 23:52:44 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="flex content-center">
+      <h1 class="text-2xl text-green-700 my-3 mx-4">Hello World</h1>
+    </div>
+    <div class="row">
+      <div class="flex items-center m-7 md6 lg4">
+        <va-card color="#0f766e" gradient>
+          <va-card-title>Title</va-card-title>
+          <va-card-content
+            ><p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ea
+              sequi cumque laboriosam, aliquid fugit voluptates dolorem et nobis
+              voluptate eaque beatae qui tempore iste est unde? Cumque, quasi
+              dolore!
+            </p>
+            <va-button class="mt-3" size="large" gradient color="#7c3aed" round>
+              Button
+            </va-button>
+          </va-card-content>
+        </va-card>
+      </div>
+    </div>
+  </div>
+</template>
